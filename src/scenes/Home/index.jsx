df3ae70e85ae3c98ab3a106ac15e5aff083543e0
Xavid Ramirez <xavidramirez@AADM08640.local>
2018-04-17 12:06:28 -0500

    adding prop-types library for typechecking

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -5,6 +5,18 @@ import Scheduler from '../../components/scheduler';
 
 class Home extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            today: new Date(),
+            hourSegments: 2, // 1 or 2 currently allowed
+            startHour: 8, // in 24-hr
+            endHour: 18, // in 24-hr
+            startDay: 1, // Monday
+            endDay: 5 // Friday
+        }
+    }
+
     render() {
         return(
             <div className="Home">
